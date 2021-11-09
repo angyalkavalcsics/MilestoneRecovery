5cd84a34e64470fda4aa8af4b5a246ea956bbf05
Angyalka Valcsics <36708283+angyalkavalcsics@users.noreply.github.com>
2021-11-09 15:37:04 -0500

    Update MilestoneRecovery.R

--- MilestoneRecovery.R
+++ MilestoneRecovery.R
@@ -2,21 +2,22 @@
 
 admitLog <- read.csv("C:/Users/angya/OneDrive/Documents/R/AdmissionLog.csv")
 admitLog <- data.frame(admitLog$DATE..IN,admitLog$TIME.IN,admitLog$DATE.OUT,
-                      admitLog$TIME..OUT,admitLog$X..DAYS,admitLog$Bed.nights)
+                       admitLog$TIME..OUT,admitLog$X..DAYS,admitLog$Bed.nights)
 colnames(admitLog) <- c("DateIn", "TimeIn", "DateOut", "TimeOut", "NumDays", 
-                       "NumNights")
+                        "NumNights")
 
 # don't really care about na in the other columns at this time
-admit = admitLog[!(is.na(admitLog$TimeOut) | admitLog$TimeOut=="" | admitLog$TimeOut == "?"),]
-admit = admit[!(is.na(admitLog$TimeIn) | admitLog$TimeIn=="" | admitLog$TimeIn == "?"),]
+admit = admitLog[!(is.na(admitLog$TimeOut) | admitLog$TimeOut=="-" | admitLog$TimeOut=="" | admitLog$TimeOut == "?" | admitLog$TimeOut == "0"),]
+admit = admit[!(is.na(admit$TimeIn) | admit$TimeIn=="" | admit$TimeIn == "?"),]
 head(admit)
+admit = admit[, 1:4]
 
 # read in non admit data
 
 rawData=read.csv("C:/Users/angya/OneDrive/Documents/R/RawData.csv")
 rawData <- data.frame(rawData[, 1:8])
 colnames(rawData) <- c("MonthYear", "Gender", "BirthYear", "ReasonForNonAdmit",
-                      "DetoxFrom", "Insurance", "Returning", "Resident")
+                       "DetoxFrom", "Insurance", "Returning", "Resident")
 head(rawData)
 
 nonadmit <- data.frame(rawData["MonthYear"], rawData["ReasonForNonAdmit"])
@@ -29,10 +30,12 @@ head(nonadmit)
 # first need to combine the data/time columns for arrival and departure
 
 sapply(admit, class)
+admit$TimeOut <- as.integer(admit$TimeOut)
+sapply(admit, class)
 
-# gotta love reformatting everything -_-
+# gotta love reformatting everything
 
-newTimeIn <- sprintf("%04d", admit$TimeIn)
+newTimeIn <- sprintf("%04s", admit$TimeIn)
 newTimeIn <- gsub("(..)(..)", "\\1:\\2", newTimeIn)
 
 newTimeOut <- sprintf("%04s", admit$TimeOut)
@@ -54,20 +57,17 @@ arrInHrs = as.numeric(as.POSIXct(admit$arrival)-as.POSIXct("2018-01-01 08:45:00"
 admit$arrival_hrs <- arrInHrs
 depInHrs = as.numeric(as.POSIXct(admit$departure)-as.POSIXct("2018-01-01 08:45:00"), units= "hours")
 admit$departure_hrs <- depInHrs
-admit$service = depInHrs - arrInHrs
+admit = admit[admit$departure_hrs >= 0, ]
+
+
+admit$service = admit$departure_hrs - admit$arrival_hrs
+
+as.numeric(as.POSIXct("2018-01-16 18:45:00")-as.POSIXct("2018-01-01 08:45:00"), units= "hours")
+
 admit$interarrivals = c(diff(arrInHrs), NA)
-
-# this data is really........
-
-admit = admit[admit$interarrivals >= 0, ]
 
 hist(admit$service) # This does not look exponential to me
 # erlang perhaps?
 hist(admit$arrival_hrs) # looks approx uniform
 hist(admit$departure_hrs) # also approx unif
 hist(admit$interarrivals)
-
-
-
-
-
